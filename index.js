e1ba803ebbbb41296f24d1d437672a1d10650508
MauricinhoMorales <mauricio1999morales@gmail.com>
2023-02-02 18:53:44 -0400

    Added the PrivateKey for Signed transacctions

--- index.js
+++ index.js
@@ -7,24 +7,38 @@ const MORALIS_API_KEY = "tGVJWE6jNqj51oLlm6L0x2uyO9IuSpickrReaKsRzgUSIhNbGAnVimr
 
 const runApp = async () => {
     
-    const privateKey = "0x8C96012dEEb2224fF9254cA00D1Cb696835CDa35";
+    // PrivateKey of the test account
+    const privateKey = "a0554bccb5a4cbd6e2f74754f66deee5410b67806361e629d1a71299abc8f6db";
+
+    //Created a SDK with signed transactions
     const sdk = ThirdwebSDK.fromPrivateKey(privateKey, "goerli");
 
-    const contract = await sdk.getContract("0x5209A9A17e0A54615D3C24C92570fB5b9B14AB1b", "token");
-    const toAddress = "0x8C96012dEEb2224fF9254cA00D1Cb696835CDa35";
-    const amount = 0.1;
-    await contract.transfer(toAddress, amount);
+    // //Created a sdk without signed transactions
+    // const sdk = new ThirdwebSDK("goerli");
 
+    // Get the contract given the address
+    const contract = await sdk.getContract("0x5209A9A17e0A54615D3C24C92570fB5b9B14AB1b", "token");
+
+    // Address of the wallet you want to mint the tokens to
+    const toAddress = "0x768e02d0b50fcBc97163CBe70285236e97Ff3001";
+
+    // The amount of this token you want to mint
+    const amount = "1";
+
+    // Function to mint 
+    await contract.mintTo(toAddress, amount);
+
+    // GET the transfers through the Moralis API
     await Moralis.start({
         apiKey: MORALIS_API_KEY,
     });
 
-    const contact_address = '0x5209a9a17e0a54615d3c24c92570fb5b9b14ab1b';
+    const contract_address = '0x5209a9a17e0a54615d3c24c92570fb5b9b14ab1b';
 
     const chain = EvmChain.GOERLI;
 
     const response = await Moralis.EvmApi.token.getTokenTransfers({
-        address: contact_address,
+        address: contract_address,
         chain,
     });
     
